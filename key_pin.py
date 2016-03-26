7c6d0fd6e9074fa2978a0247bd68b23ae1000e4a
peoplma <peoplma@gmail.com>
2016-03-25 20:32:10 -0400

    Update key_pin.py

--- key_pin.py
+++ key_pin.py
@@ -1,3 +1,3 @@
-blockio_api_key = #blockio_api_key
-blockio_secret_pin = #blockio_secret_pin
-slack_token = #slackAPItoken
+blockio_api_key = #"blockio_api_key"
+blockio_secret_pin = #"blockio_secret_pin"
+slack_token = #"slackAPItoken"
