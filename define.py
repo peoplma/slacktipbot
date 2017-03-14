1a56ee578d7ca4cd5574899db90fe6e6063ee4aa
JeJe <jeje@ggcorp.fr>
2017-03-14 22:38:17 +0100

    move some define

---
+++ define.py
@@ -0,0 +1,9 @@
+url = 'https://shapeshift.io/shift'
+coincap_doge = 'http://www.coincap.io/page/DOGE'
+coincap_btc = 'http://www.coincap.io/page/BTC'
+coincap_ltc = 'http://www.coincap.io/page/LTC'
+cryptocomp_doge = 'https://www.cryptocompare.com/api/data/price?fsym=DOGE&tsyms=USD'
+cryptocomp_btc = 'https://www.cryptocompare.com/api/data/price?fsym=BTC&tsyms=USD'
+cryptocomp_ltc = 'https://www.cryptocompare.com/api/data/price?fsym=LTC&tsyms=USD'
+shapeshift_pubkey = "06c04cfc9f18632d50ca546ba4f3dc49edcaf6217e3cefe73ed98d92cc2f37e764df8371fc3d23847aee4a4d65bdaa2defd30ca43311378827a94146feb017cb"
+min_amount = {'doge': 2.0, 'ltc': 0.002, 'btc': 0.0002}
--- slacktipbot.py
+++ slacktipbot.py
@@ -9,20 +9,13 @@ from slackclient import SlackClient
 from block_io import BlockIo
 version = 2 # API version
 from key_pin import *
-block_io_doge = BlockIo(blockio_api_doge_key, blockio_secret_pin, version) 
+from define import *
+
+block_io_doge = BlockIo(blockio_api_doge_key, blockio_secret_pin, version)
 block_io_btc = BlockIo(blockio_api_btc_key, blockio_secret_pin, version)
 block_io_ltc = BlockIo(blockio_api_ltc_key, blockio_secret_pin, version)
 ss = SlackSocket(slack_token,translate=False) # translate will lookup and replace user and channel IDs with their human-readable names. default true. 
 sc = SlackClient(slack_token)
-url = 'https://shapeshift.io/shift'
-coincap_doge = 'http://www.coincap.io/page/DOGE'
-coincap_btc = 'http://www.coincap.io/page/BTC'
-coincap_ltc = 'http://www.coincap.io/page/LTC'
-cryptocomp_doge = 'https://www.cryptocompare.com/api/data/price?fsym=DOGE&tsyms=USD'
-cryptocomp_btc = 'https://www.cryptocompare.com/api/data/price?fsym=BTC&tsyms=USD'
-cryptocomp_ltc = 'https://www.cryptocompare.com/api/data/price?fsym=LTC&tsyms=USD'
-shapeshift_pubkey = "06c04cfc9f18632d50ca546ba4f3dc49edcaf6217e3cefe73ed98d92cc2f37e764df8371fc3d23847aee4a4d65bdaa2defd30ca43311378827a94146feb017cb"
-min_amount = {'doge': 2.0, 'ltc': 0.002, 'btc': 0.0002}
 
 def main():
 	time.sleep(1)
